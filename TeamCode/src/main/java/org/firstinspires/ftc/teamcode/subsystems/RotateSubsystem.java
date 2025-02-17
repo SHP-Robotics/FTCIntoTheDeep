b91d256dee0ea868f48641ca068f4dfe8b1950ac
kho25 <91566656+kho25@users.noreply.github.com>
2025-02-16 23:45:23 -0800

    fixed rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RotateSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/RotateSubsystem.java
@@ -59,9 +59,8 @@ public class RotateSubsystem extends Subsystem {
     }
     public void processState() {
         if(this.state == State.INTAKE)
-            return;
-
-        if (this.state == State.NEUTRAL || this.state == State.DROPOFF)
+            rotate.setPosition(rotatePos);
+        else if (this.state == State.NEUTRAL || this.state == State.DROPOFF)
             rotatePos = kNeutral;
         else if (this.state == State.PICKUP)
             rotatePos = kPickup;
@@ -70,7 +69,6 @@ public class RotateSubsystem extends Subsystem {
         else if (this.state == State.SAMPLE)
             rotatePos = 0.9; // 1;
 
-        rotate.setPosition(rotatePos);
     }
 
     @Override
